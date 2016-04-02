41638f98ade999e9b34080ae2a1348e17e668371
Jarrod Bennie <jarrodbennie@icloud.com>
2016-04-02 20:44:02 +0100

    moves variable definitions to controller

--- controllers/main_controller.rb
+++ controllers/main_controller.rb
@@ -4,5 +4,6 @@ require_relative('../models/account')
 get "/" do
   options = { "transactions" => Transaction.all, "merchants" => Merchant.all, "tags" => Tag.all, "income" => Income.all, "targets" => Target.all }
   @account = Account.new(options)
+  @target = @account.targets[ 0 ]
   erb :main
 end
