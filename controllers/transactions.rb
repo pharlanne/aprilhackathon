4249cd0471c889e8e1a448c64cbbf9e9714884b1
Jarrod Bennie <jarrodbennie@icloud.com>
2016-04-03 00:35:36 +0100

    adds test text for views

--- controllers/transactions.rb
+++ controllers/transactions.rb
@@ -1,13 +1,82 @@
+require "sinatra"
+require "sinatra/contrib/all"
 require_relative "../models/account"
 
-get 'transactions/new' do
-  options = { 'transactions' => Transaction.all, "merchants" => Merchants.all, "tags" => Tags.all, "income" => Income.all }
-  @account = Account.new(options)
-  erb :"transactions/new"
+# Display all transactions.
+get "/transactions" do
+  if params[ :filter ] == "month"
+    options = {
+      "transactions" => Transaction.find_this_month,
+      "merchants" => Merchant.all,
+      "tags" => Tag.all,
+      "income" => Income.all,
+      "targets" => Target.all
+    }
+  else
+  options = {
+    "transactions" => Transaction.all,
+    "merchants" => Merchant.all,
+    "tags" => Tag.all,
+    "income" => Income.all,
+    "targets" => Target.all
+  }
+  end
+  @account = Account.new( options )
+  if @account.transactions.size == 0
+    erb :"/transactions/empty"
+  else
+    erb :"/transactions/index"
+  end
 end
 
-post 'transactions' do
-  @transaction = Transaction.new
-  @transaction.create
-  redirect to ('transactions/new')
+# Create a new transaction.
+get "/transactions/new" do
+  options = {
+    "transactions" => Transaction.all,
+    "merchants" => Merchant.all,
+    "tags" => Tag.all,
+    "income" => Income.all,
+    "targets" => Target.all
+  }
+  @account = Account.new( options )
+  erb :"/transactions/new"
+end
+
+# Display additional information about and all transactions for a single transaction.
+get "/transactions/:id" do
+  @transaction = Transaction.find( params[ :id ])
+  erb :"/transactions/show"
+end
+
+# Post new transaction.
+post "/transactions" do
+  @transaction = Transaction.create( params )
+  redirect to "/transactions"
+end
+
+# Edit a transaction.
+get "/transactions/:id/edit" do
+  @transaction = Transaction.find( params[ :id ])
+  options = {
+    "transactions" => Transaction.all,
+    "merchants" => Merchant.all,
+    "tags" => Tag.all,
+    "income" => Income.all,
+    "targets" => Target.all
+  }
+  @account = Account.new( options )
+  erb :"/transactions/edit"
+end
+
+# Post edited transaction.
+post "/transactions/:id" do
+  @transaction = Transaction.new( params )
+  @transaction.update
+  redirect to "/transactions"
+end
+
+# Post deleted transaction.
+post "/transactions/:id/delete" do
+  Transaction.destroy( params[ :id ])
+  redirect to "/transactions"
 end
